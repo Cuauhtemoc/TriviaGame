67f074fbd3a9b0ea70bdded1d68cd300aba0e956
Cuauhtemoc Rodriguez <crodri606@gmail.com>
2019-01-10 16:16:39 -0600

    added counters to end screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,31 +4,31 @@ var currenQuestion = -1;
 var correctAnswer;
 var numberCorrect = 0;
 var numberIncorrect = 0;
+var currentTime = 0;
 var timer;
-
+var timerOnscreen;
+var startTimer = false;
 getData("https://opentdb.com/api.php?amount=20&category=11&difficulty=easy&type=multiple", function(d){
     TriviaQuestions = d;
 });
 getData("http://api.giphy.com/v1/gifs/search?q=cute+dogs&api_key=6eo1uTgLl9IbW6ustLLdSjQ5wh4RH9XM", function(d){
 gifs = d;
 });
-
-function getData (url, callback) {
+function getData (url, captureData) {
     var xhr = new XMLHttpRequest;
     xhr.open("GET", url);
-
     xhr.onload = function(){
     console.log(xhr.status); 
     var data;   
     data = JSON.parse(this.responseText);
-    callback(data); 
-    console.log(data);
+    captureData(data); 
     }
     xhr.send();
 }
 function displayGame (){
-        console.log(numberCorrect);
-        console.log(numberIncorrect);
+        startTimer = true;
+        startTimer = true;
+        currentTime = 20;
         if (currenQuestion >= TriviaQuestions.results.length -1)
         {
             endGame();
@@ -39,14 +39,14 @@ function displayGame (){
         $('#gameStart').hide();
         $('#choices').empty();
         $("#question").empty();
+        $("#timer").html("<h1> Time Remaining: " + currentTime + "</h1>");
         var q = $("<h1>");
         var choice = []
         q.html(TriviaQuestions.results[currenQuestion].question);
         $("#question").append(q)
         for (i = 0; i < TriviaQuestions.results[currenQuestion].incorrect_answers.length; i++)
         {
-            choice.push(TriviaQuestions.results[currenQuestion].incorrect_answers[i]);
-            
+            choice.push(TriviaQuestions.results[currenQuestion].incorrect_answers[i]);            
         }
         choice.push(TriviaQuestions.results[currenQuestion].correct_answer);
         shuffle(choice);
@@ -55,16 +55,19 @@ function displayGame (){
         {
             $("#choices").append(("<button id = 'answer'>" + choice[i] + "</button>"));
         }
-        timer = setTimeout(timesUp, 20000);
+        timer = setTimeout(timesUp, currentTime * 1000);
+        timerOnscreen = setInterval(displayTime, 1000);
     }
 } ;
 function timesUp(){ 
     numberIncorrect++;
+    currentTime = 5;
     $('#choices').empty();
     $("#question").empty();
     $("#choices").html("<h3> Times Up. The correct answer was: " + correctAnswer + "</h3>");
     clearTimeout(timer);
-    setTimeout(displayGame, 5000);
+    clearInterval(timerOnscreen);
+    setTimeout(displayGame, currentTime * 1000);
 }
 function endGame(){
     $('#choices').empty();
@@ -96,18 +99,20 @@ function shuffle (array){
 };
 function win() {
     var winnerGif = $("<img>");
-    winnerGif.attr("src", gifs.data[currenQuestion].images.downsized.url);
+    currentTime = 5;
+    winnerGif.attr("src", gifs.data[currenQuestion].images.fixed_width.url);
     $('#choices').empty();
     $("#question").empty();
     $("#question").html("<h3> Thats correct! Here's a cute dog! </h3>");
     $("#choices").html(winnerGif);
     numberCorrect++;
     clearTimeout(timer);
-    setTimeout(displayGame, 5000);
-
+    clearInterval(timerOnscreen);
+    setTimeout(displayGame, currentTime * 1000);
 }
 function lose(){
     var winnerGif = $("<img>");
+    currentTime = 5;
     winnerGif.attr("src", gifs.data[currenQuestion].images.downsized.url);
     $('#choices').empty();
     $("#question").empty();
@@ -115,7 +120,16 @@ function lose(){
     $("#choices").html(winnerGif);
     numberIncorrect++;
     clearTimeout(timer);
-    setTimeout(displayGame, 5000);
+    clearInterval(timerOnscreen);
+    setTimeout(displayGame, currentTime * 1000);
+}
+function displayTime(){
+    currentTime--;
+    if (currentTime >= 0)
+    {
+    $("#timer").html("<h1> Time Remaining: " + currentTime + "</h1>");
+    console.log(currentTime);
+    }
 }
 $("#gameStart").on("click", displayGame);
 $("#choices").on("click", "#answer", function (e){
@@ -127,7 +141,6 @@ $("#choices").on("click", "#answer", function (e){
     }
     else 
         lose();
-
 })
 
 
